215f88e42c14ac6809c3817a997218d3aacc1d98
rendhive <rendhivecode@gmail.com>
2026-06-17 13:19:51 +0700

    [DOCT] Sample Library Python: 1. Mengimpor Modul html.entities.py

---
+++ 1. Mengimpor Modul html.entities.py
@@ -0,0 +1,5 @@
+import html.entities
+
+print("Modul html.entities berhasil diimpor.")
+# Fungsi: Mengimpor modul untuk menggunakan mappings karakter HTML.
+# Kondisi: Digunakan sebelum memanfaatkan karakter HTML dalam kode.
