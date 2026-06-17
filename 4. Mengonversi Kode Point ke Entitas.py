c15aae78a651092b7b6658f173f6af8ec61f5568
rendhive <rendhivecode@gmail.com>
2026-06-17 13:33:30 +0700

    [DOCT] Sample Library Python: 4. Mengonversi Kode Point ke Entitas.py

---
+++ 4. Mengonversi Kode Point ke Entitas.py
@@ -0,0 +1,5 @@
+import html.entities
+
+print("Entitas untuk kode point 60:", html.entities.codepoint2name.get(60))  # Kode untuk '<'
+# Fungsi: Mengonversi kode point menjadi entitas HTML.
+# Kondisi: Ketika Anda memiliki nilai Unicode dan ingin menemukan entitas HTML yang sesuai.
